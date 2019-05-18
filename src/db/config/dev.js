cdbcf3dfb3c6806a5fa86dfb7d42edcc1c330b99
Bilal Kazmi <39033434+bilu7111@users.noreply.github.com>
2019-05-19 04:39:23 +0500

    Update dev.js

--- src/db/config/dev.js
+++ src/db/config/dev.js
@@ -1,7 +1,7 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyBNzaUtLT0JSnqVKtAabUyKK7itSdJ_LaE",
-  authDomain: "budget-management-ui.firebaseapp.com",
-  databaseURL: "https://budget-management-ui.firebaseio.com",
-  storageBucket: "gs://budget-management-ui.appspot.com",
-  messagingSenderId: '597469023830'
+  apiKey: "API_KEY",
+  authDomain: "DOMAIN",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ''
 };
